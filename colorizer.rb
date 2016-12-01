aaf8edfbbdc26f5f4593a9ee2721b40d2788e27e
Artur Jaworski <kontakt@arturjaworski.pl>
2016-12-01 10:25:30 +0100

    Colorizer 0.1.1

--- colorizer.rb
+++ colorizer.rb
@@ -1,8 +1,8 @@
 class Colorizer < Formula
   desc "A Swift tool to generate Apple's Color Palette file (.clr file) from .txt file."
   homepage "https://github.com/arturjaworski/Colorizer"
-  url "https://github.com/arturjaworski/Colorizer/releases/download/0.1/colorizer-0.1.zip"
-  sha256 "2bb61bb175e02fb7526ac7dec981dfb1fbc3f723aec596f1f19516134c8ad5b3"
+  url "https://github.com/arturjaworski/Colorizer/releases/download/0.1.1/colorizer-0.1.1.zip"
+  sha256 "b279f63d442d7a3c673fa3b6fb4b6e7715c6cdcea8d452a9c1e6210d0e290469"
   head "https://github.com/arturjaworski/Colorizer.git"
 
   def install
